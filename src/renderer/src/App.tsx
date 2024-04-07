5444b79bf83059f0e96f4cc3ddc1de26aa4871ff
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-04-07 01:45:24 -0500

    tanstack router setup and configuration complete

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,20 @@
-import { Button } from '@renderer/@/components/ui/button'
+import { Button } from './@/components/ui/button'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 function App(): JSX.Element {
   return (
