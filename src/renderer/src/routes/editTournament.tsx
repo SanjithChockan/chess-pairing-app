84ddc4fb65bb9ec97ec56a2833341dcdd44f8b3b
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-05-20 19:11:58 -0500

    implemented type checking

--- src/renderer/src/routes/editTournament.tsx
+++ src/renderer/src/routes/editTournament.tsx
@@ -1,7 +1,17 @@
 import { Link, createFileRoute } from '@tanstack/react-router'
 
+type tournamentSearch = {
+  tourneyName: string
+}
+
 export const Route = createFileRoute('/editTournament')({
-  component: editTournament
+  component: editTournament,
+  validateSearch: (search: Record<string, unknown>): tournamentSearch => {
+    // validate and parse the search params into a typed state
+    return {
+      tourneyName: (search.filter as string) || ''
+    }
+  }
 })
 
 function editTournament(): JSX.Element {
