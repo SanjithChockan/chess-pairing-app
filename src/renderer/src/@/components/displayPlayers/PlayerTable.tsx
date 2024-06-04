024b526e5babeada67fe8d1b5a9e5a0eb0a464cb
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-06-04 13:52:17 -0500

    added dialog functionality for user input. need to extract info from user input to table

--- src/renderer/src/@/components/displayPlayers/PlayerTable.tsx
+++ src/renderer/src/@/components/displayPlayers/PlayerTable.tsx
@@ -3,6 +3,19 @@ import { useCallback, useState } from 'react'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-quartz.css'
 import { Button } from '@renderer/@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose
+} from '@renderer/@/components/ui/dialog'
+import { Input } from '@renderer/@/components/ui/input'
+import { Label } from '@renderer/@/components/ui/label'
+
 let data: object[] = [
   {
     firstName: 'testfn1',
@@ -40,6 +53,42 @@ export default function PlayerGrid(): JSX.Element {
       </div>
 
       <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      <div>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Add Player</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Player Details</DialogTitle>
+              <DialogDescription>
+                Enter new player information here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="firstname" className="text-right">
+                  First Name
+                </Label>
+                <Input id="firstname" defaultValue="Pedro" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="lastname" className="text-right">
+                  Last Name
+                </Label>
+                <Input id="lastname" defaultValue="Duarte" className="col-span-3" />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type="submit" onClick={addPlayer}>
+                  Enter
+                </Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   )
 }
