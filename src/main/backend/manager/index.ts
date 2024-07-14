a1165999704284c980daed422a3db0ddfdbf0e8a
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-07-14 12:06:41 -0400

    added new db. remember to create TourneyList

--- src/main/backend/manager/index.ts
+++ src/main/backend/manager/index.ts
@@ -4,16 +4,7 @@ import Database from 'better-sqlite3'
 
 export default function getManagerObj(): Manager {
   // connect to db
-  const db_path = path.join(
-    __dirname,
-    '..',
-    '..',
-    'src',
-    'main',
-    'backend',
-    'db',
-    'experimental.db'
-  )
+  const db_path = path.join(__dirname, '..', '..', 'src', 'main', 'backend', 'db', 'pairing.db')
 
   const db = new Database(db_path)
   const managerObj = new Manager(db, '', '')
