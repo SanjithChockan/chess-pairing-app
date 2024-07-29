875aaffd94a6d7e3ed1cc95fee5fb12836244a6c
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-07-28 22:36:51 -0500

    tested. double check rerendering when deleting player from table

--- src/renderer/src/routes/tournamentCompleteView/$tourneyName.tsx
+++ src/renderer/src/routes/tournamentCompleteView/$tourneyName.tsx
@@ -1,4 +1,7 @@
+import { AgGridReact } from 'ag-grid-react'
+import { useRef, useState, useCallback } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { ColumnState, GridReadyEvent } from '@ag-grid-community/core'
 
 export const Route = createFileRoute('/tournamentCompleteView/$tourneyName')({
   component: finalView
@@ -6,11 +9,42 @@ export const Route = createFileRoute('/tournamentCompleteView/$tourneyName')({
 
 function finalView(): JSX.Element {
   const { tourneyName } = Route.useParams()
+  const gridRef = useRef<AgGridReact>(null)
+  const [playersList, setPlayersState] = useState([
+    { firstName: '', lastName: '', rating: 0, score: 0 }
+  ])
+  const [rowData, setRowData] = useState(playersList)
+  const [colDefs, setColDefs] = useState([
+    { field: 'firstName' },
+    { field: 'lastName' },
+    { field: 'rating', sortable: true },
+    { field: 'score', sortable: true }
+  ])
 
+  const onGridReady = useCallback(async (params: GridReadyEvent) => {
+    console.log(`tourneyName in Current Standings ${tourneyName}`)
+    const names = await window.api.getCurrentStandings(tourneyName)
+    setPlayersState(names)
+    setRowData(names)
+    const defaultSortModel: ColumnState[] = [
+      { colId: 'score', sort: 'desc', sortIndex: 0 },
+      { colId: 'rating', sort: 'desc', sortIndex: 1 }
+    ]
+    params.api.applyColumnState({ state: defaultSortModel })
+  }, [])
   return (
     <>
       <div>
         <h1>{tourneyName} Final Standings </h1>
+      </div>
+      <div className="ag-theme-quartz w-full" style={{ height: 500 }}>
+        <AgGridReact
+          ref={gridRef}
+          rowData={rowData}
+          columnDefs={colDefs}
+          rowSelection={'multiple'}
+          onGridReady={onGridReady}
+        />
       </div>
     </>
   )
