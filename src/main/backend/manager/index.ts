e37c844f4fc950a3e6197fa3e74c445e1142eba8
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-08-16 14:48:57 -0500

    complete. need to package

--- src/main/backend/manager/index.ts
+++ src/main/backend/manager/index.ts
@@ -4,7 +4,7 @@ import Database from 'better-sqlite3'
 
 export default function getManagerObj(): Manager {
   // connect to db
-  const db_path = path.join(__dirname, '..', '..', 'src', 'main', 'backend', 'db', 'pairing.db')
+  const db_path = path.join(__dirname, '..', '..', 'src', 'main', 'backend', 'db', 'production.db')
 
   const db = new Database(db_path)
   const managerObj = new Manager(db, '', '')
